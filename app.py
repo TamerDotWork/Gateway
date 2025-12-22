2a9a9631e2ec7e535c3caa3182017af17cef4864
TamerDotWork <74485569+TamerDotWork@users.noreply.github.com>
2025-12-22 09:43:15 +0200

    py

--- app.py
+++ app.py
@@ -1,16 +1,3 @@
-from google import genai
-
-
-
-client = genai.Client(api_key="YOUR_API_KEY")
-
-response = client.models.generate_content(
-    model="gemini-flash-latest", 
-    contents="What is the capital of Italy?"
-)
-
-print(response.text)
-
 import requests
 
 # Point to your Middleman/Gateway instead of Google

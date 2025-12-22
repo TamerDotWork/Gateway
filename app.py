b9543bfd83955a558a213df21bcb2ba5cd8711a4
TamerDotWork <74485569+TamerDotWork@users.noreply.github.com>
2025-12-22 08:39:44 +0200

    py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ client = genai.Client(api_key="YOUR_API_KEY")
 
 response = client.models.generate_content(
     model="gemini-flash-latest", 
-    contents="What is the capital of France?"
+    contents="What is the capital of Italy?"
 )
 
 print(response.text)
